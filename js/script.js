9c896402c66c85c98f6a71834f52adb7d6a4705b
Liliia Pavliuchenkova <liliiapav@gmail.com>
2024-03-20 15:16:14 -0700

    window load

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-const $ = (id) => document.getElementById(id);
+window.addEventListener("load", () => {
+  const $ = (id) => document.getElementById(id);
+  let fmEmpl = $("empForm");
+  let valEmplId = $("id");
+  let valEmplName = $("name");
+  let valEmplExt = $("ext");
+  let valEmplEmail = $("email");
+  let valEmplDep = $("department");
 
-let fmEmpl = $("empForm");
-let valEmplId = $("id");
-let valEmplName = $("name");
-let valEmplExt = $("ext");
-let valEmplEmail = $("email");
-let valEmplDep = $("department");
-
-fmEmpl.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("ID:", valEmplId.value);
-  console.log("Name:", valEmplName.value);
-  console.log("Extension:", valEmplExt.value);
-  console.log("Email:", valEmplEmail.value);
-  console.log("Department:", valEmplDep.value);
-  fmEmpl.reset();
+  fmEmpl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("ID:", valEmplId.value);
+    console.log("Name:", valEmplName.value);
+    console.log("Extension:", valEmplExt.value);
+    console.log("Email:", valEmplEmail.value);
+    console.log("Department:", valEmplDep.value);
+    fmEmpl.reset();
+  });
 });
